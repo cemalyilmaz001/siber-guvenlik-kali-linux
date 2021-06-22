50f704fca2fad31f8f77eae1aa42a8082ac7819f
Cemal YILMAZ <69977605+cemalyilmaz001@users.noreply.github.com>
2021-06-23 02:49:20 +0300

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+xfce4-terminal -H -x sudo tail -f ufw.log |
+xfce4-terminal -H -x sudo tail -f utmp |
+xfce4-terminal -H -x sudo tail -f lastlog  |
+xfce4-terminal -H -x sudo tail -f auth.log |
+xfce4-terminal -H -x sudo tail -f syslog  |
+xfce4-terminal -H -x sudo tail -f wtmp |
+echo  " İSLEM DEVAM EDİYOR "
